7623bbc9b92947cc13bf2d455b51f929ca1e8205
leiless <38041294+leiless@users.noreply.github.com>
2020-10-29 22:46:08 +0800

    build-redsocks.sh: Build for debug/release redsocks2

--- build-redsocks.sh
+++ build-redsocks.sh
@@ -7,11 +7,15 @@ cd "$(dirname "$0")"
 
 rm -rf redsocks
 rm -f redsocks2
+rm -f redsocks2-debug
 git clone --depth 1 https://github.com/semigodking/redsocks
 
 pushd redsocks
 git apply ../patch_redsocks.diff
-make DISABLE_SHADOWSOCKS=true ENABLE_HTTPS_PROXY=true -j$(sysctl -n hw.ncpu)
-mv redsocks2 ..
+make debug DISABLE_SHADOWSOCKS=true ENABLE_HTTPS_PROXY=true -j$(sysctl -n hw.ncpu)
+mv redsocks2 ../redsocks2-debug
+make clean
+make release DISABLE_SHADOWSOCKS=true ENABLE_HTTPS_PROXY=true -j$(sysctl -n hw.ncpu)
+mv redsocks2 ../redsocks2
 popd
 
